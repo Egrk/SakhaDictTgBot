6a9c3342eb54b36598307980ea72a9ca82ac6e00
gavriliev.egor <gavriliev.egor.opus@gmail.com>
2023-04-24 18:35:45 +0400

    Create goprher balancer

---
+++ dataParser.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"container/list"
+)
+
+var counter = 0
+var callback = make(chan int)
+var queue = list.New()
+
+func balancer(upstream chan pack) {
+		for {
+			select {
+			case newPack := <- upstream:
+				if counter < 5 {
+					go sentenceParser(newPack, callback)
+					counter++
+				} else {
+					queue.PushBack(newPack)
+				}
+			case back := <- callback:
+				counter -= back
+				if queue.Len() != 0 {
+					for counter < 5 {
+						frontElement := queue.Front()
+						if frontElement == nil {
+							break
+						}
+						go sentenceParser(frontElement.Value.(pack), callback)
+						counter++
+						queue.Remove(frontElement)
+					}
+				}
+		}
+	}
+}
