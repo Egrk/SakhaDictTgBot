fa8d5ec4ba7f7223b6d4094bf0559897ce7e0505
gavriliev.egor <gavriliev.egor.opus@gmail.com>
2023-06-20 16:29:38 +0400

    Deprecate balancer

--- dataParser.go
+++ dataParser.go
@@ -5,7 +5,7 @@ import (
 )
 
 var counter = 0
-var callback = make(chan int)
+var callback = make(chan pack)
 var queue = list.New()
 
 func balancer(upstream chan pack) {
@@ -18,19 +18,19 @@ func balancer(upstream chan pack) {
 				} else {
 					queue.PushBack(newPack)
 				}
-			case back := <- callback:
-				counter -= back
-				if queue.Len() != 0 {
-					for counter < 5 {
-						frontElement := queue.Front()
-						if frontElement == nil {
-							break
-						}
-						go sentenceParser(frontElement.Value.(pack), callback)
-						counter++
-						queue.Remove(frontElement)
-					}
-				}
+			// case back := <- callback:
+			// 	counter -= back
+			// 	if queue.Len() != 0 {
+			// 		for counter < 5 {
+			// 			frontElement := queue.Front()
+			// 			if frontElement == nil {
+			// 				break
+			// 			}
+			// 			go sentenceParser(frontElement.Value.(pack), callback)
+			// 			counter++
+			// 			queue.Remove(frontElement)
+			// 		}
+			// 	}
 		}
 	}
 }
