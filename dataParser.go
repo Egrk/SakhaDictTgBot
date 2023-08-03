06577f0ebf37ff1dfe12a30e65de6dc9689bba16
gavriliev.egor <gavriliev.egor.opus@gmail.com>
2023-08-03 19:09:46 +0400

    Give var's pretty names

--- dataParser.go
+++ dataParser.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"container/list"
 	"strconv"
 	"unicode"
 	"unicode/utf8"
@@ -12,9 +11,9 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-var counter = 0
-var callback = make(chan pack)
-var queue = list.New()
+// var counter = 0
+// var callback = make(chan pack)
+// var queue = list.New()
 
 type pack struct {
 	wordExplain word
@@ -33,92 +32,64 @@ type searchSettings struct {
 	chatID int64
 }
 
-func sentenceParser(pack pack, callback chan pack) {
-	runeList := []rune(pack.wordExplain.rawData)
-	var lastSentence []string
-	check := false
-	lenOfList := len(runeList)
-	var ans []string
-	indx := 0
-	number := 1
-	for indx < lenOfList {
-		if runeList[indx] == 'Θ' {
-			idnxStep, nextSentence := firstSentenceParse(runeList[indx+1:])
-			indx += idnxStep
-			firstSentence := strings.Join(lastSentence, "")
-			ans = append(ans, strconv.Itoa(number)+") "+firstSentence+"Θ "+nextSentence)
-			number++
-		}
-		if check {
-			if indx+2 < lenOfList {
-				if runeList[indx] == '.' && (unicode.IsUpper(runeList[indx+2]) || runeList[indx+2] == ' ') {
-					check = false
-				}
-			}
-			lastSentence = append(lastSentence, string(runeList[indx]))
-		} else if unicode.IsUpper(runeList[indx]) {
-			lastSentence = nil
-			lastSentence = append(lastSentence, string(runeList[indx]))
-			check = true
-		}
-		indx++
+func balancer(upstream <-chan searchSettings) {
+	var semafore = make(chan struct{}, 15)
+	for elem := range upstream {
+		semafore <-struct{}{}
+		go parseHtmlBody(elem.raw, elem.chatID, semafore)
 	}
-	if len(ans) != 0 {
-		pack.wordExplain.texts = ans
-	}
-	callback <- pack
 }
 
 func parseHtmlBody(text []byte, chatID int64, done <-chan struct{}) {
 	defer func ()  {
 		<-done
 	}()
-	tkn := html.NewTokenizer(bytes.NewReader(text))
+	tokenizer := html.NewTokenizer(bytes.NewReader(text))
 	wordStruct := word{}
-	var vals []string
+	var rawData []string
 	callback := make(chan pack)
 	num := 0
 	packet := pack{
 		chatID: chatID,
 	}
 	for {
-		tt := tkn.Next()
-		if tt == html.ErrorToken {
+		tokenType := tokenizer.Next()
+		if tokenType == html.ErrorToken {
 			break
 		}
-		if tt != html.StartTagToken {
+		if tokenType != html.StartTagToken {
 			continue
 		}
-		t := tkn.Token()
-		if t.Data == "div" && t.Attr[0].Key == "class" && t.Attr[0].Val == "text" {
+		token := tokenizer.Token()
+		if token.Data == "div" && token.Attr[0].Key == "class" && token.Attr[0].Val == "text" {
 			Loop:
 			for {
-				switch tkn.Next() {
+				switch tokenizer.Next() {
 				case html.TextToken: 
-					t := tkn.Token()
-					if strings.TrimSpace(t.Data) != "" {
-						vals = append(vals, t.Data)
+					token := tokenizer.Token()
+					if strings.TrimSpace(token.Data) != "" {
+						rawData = append(rawData, token.Data)
 					}
 				case html.EndTagToken:
-					t := tkn.Token()
+					t := tokenizer.Token()
 					if t.Data == "div" {
 						break Loop
 					}
 				}
 			}
-			wordStruct.head = vals[0]
-			wordStruct.rawData = strings.Join(vals[1:], "")
+			wordStruct.head = rawData[0]
+			wordStruct.rawData = strings.Join(rawData[1:], "")
 			packet.wordExplain = wordStruct
 			packet.number = num
 			num++
 			go sentenceParser(packet, callback)
 		}
-		vals = nil
+		rawData = nil
 	}
-	packArray := make([]pack, num)
+	packSlice := make([]pack, num)
 	for i := 0; i < num; i++ {
-		if packArray[i].chatID != 0 {
-			iterateAndSend(packArray[i])
+		if packSlice[i].chatID != 0 {
+			iterateAndSend(packSlice[i])
 			continue
 		}
 		for {
@@ -127,12 +98,48 @@ func parseHtmlBody(text []byte, chatID int64, done <-chan struct{}) {
 				iterateAndSend(handledPack)
 				break
 			}
-			packArray[handledPack.number] = handledPack
+			packSlice[handledPack.number] = handledPack
 		}
 	}
 }
 
-func firstSentenceParse(text []rune) (int, string) {
+func sentenceParser(pack pack, callback chan pack) {
+	runeList := []rune(pack.wordExplain.rawData)
+	var lastSentence []string
+	isSentenceEnd := false
+	lenOfList := len(runeList)
+	var text []string
+	indx := 0
+	number := 1
+	for indx < lenOfList {
+		if runeList[indx] == 'Θ' {
+			indxStep, nextSentence := nextSentenceParse(runeList[indx+1:])
+			indx += indxStep
+			firstSentence := strings.Join(lastSentence, "")
+			text = append(text, strconv.Itoa(number)+") "+firstSentence+"Θ "+nextSentence)
+			number++
+		}
+		if isSentenceEnd {
+			if indx+2 < lenOfList {
+				if runeList[indx] == '.' && (unicode.IsUpper(runeList[indx+2]) || runeList[indx+2] == ' ') {
+					isSentenceEnd = false
+				}
+			}
+			lastSentence = append(lastSentence, string(runeList[indx]))
+		} else if unicode.IsUpper(runeList[indx]) {
+			lastSentence = nil
+			lastSentence = append(lastSentence, string(runeList[indx]))
+			isSentenceEnd = true
+		}
+		indx++
+	}
+	if len(text) != 0 {
+		pack.wordExplain.texts = text
+	}
+	callback <- pack
+}
+
+func nextSentenceParse(text []rune) (int, string) {
 	var ans []string
 	idx := 0
 	for i := 0; i < len(text); i++ {
@@ -160,7 +167,7 @@ func iterateAndSend(pack pack) {
 	if len(pack.wordExplain.texts) > 0 {
 		text := pack.wordExplain.head + "\n"
 		for _, wordBody := range pack.wordExplain.texts {
-			if utf8.RuneCountInString(text)+utf8.RuneCountInString(wordBody+"\n") < 4096 {
+			if utf8.RuneCountInString(text)+utf8.RuneCountInString(wordBody+"\n") < 4096 { 
 				text += wordBody + "\n"
 			} else {
 				sendMessage(text, pack.chatID)
@@ -187,12 +194,4 @@ func sendHtmlChunkWithText(body []byte, searchWord string, id int64) {
 	}
 	msg := tgbotapi.NewDocument(id, file)
 	bot.Send(msg)
-}
-
-func balancer(upstream <-chan searchSettings) {
-	var semafore = make(chan struct{}, 15)
-	for elem := range upstream {
-		semafore <-struct{}{}
-		go parseHtmlBody(elem.raw, elem.chatID, semafore)
-	}
 }
